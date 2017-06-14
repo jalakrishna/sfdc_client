a2e3b5f85f9c47491dbbd814c426aec8ae7ea147
rakesh tigulla <rtigulla@salesforce.com>
2017-06-14 12:48:42 -0400

    Added Config hash map and plugin to jar with dependencies

---
+++ ServiceLayer/src/main/com/salesforce/automation/servicelayer/config/FrameworkConfig.java
@@ -0,0 +1,23 @@
+package com.salesforce.automation.servicelayer.config;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * This interface contains all the global constants required to run automation
+ * Created by rtigulla on 6/8/17.
+ */
+public class FrameworkConfig {
+    public final static String UI_TEST_URL = "https://test.salesforce.com";
+    private static Hashtable<String,String> ConfigMap;
+
+    public static Hashtable<String,String> getConfigMap(){
+        if(null != ConfigMap){
+            ConfigMap =  new Hashtable<String,String>();
+        }
+        return  ConfigMap;
+
+    }
+
+
+}
